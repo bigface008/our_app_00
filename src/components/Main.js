73ec34161fdbab267cacff97e2524293e8e97c31
bigface008 <bigface00@sjtu.edu.cn>
2017-12-16 22:24:00 +0800

    Working...

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,23 +3,23 @@ require('styles/App.css');
 
 import React from 'react';
 
-// let yeomanImage = require('../images/yeoman.png');
 let timeArray = [5, 45, 45];
-const EXP_MIN = 1000;
-// let users = [0, 0, 0];
+const EXP_MIN = 1000; // should be changed to 1000 * 60 when handed in
 
 class Washer extends React.Component {
   constructor() {
     super();
     this.state = {
-      // 0 - not being used
+      // 0 - not being used && no clothes in washer
       // 1 - someone else is using it
       // 2 - you are using it
+      // 3 - not being used && clothes in washer
       user: 0,
       text: 'Not being used.',
       time: 0
     };
     this.handleClickOn = this.handleClickOn.bind(this);
+    this.handelClickGetClothes = this.handelClickGetClothes.bind(this);
     this.timeOn = this.timeOn.bind(this);
     this.tick = this.tick.bind(this);
   }
@@ -31,18 +31,28 @@ class Washer extends React.Component {
 
   timeOn() {
     let mins = timeArray[this.props.order];
+    this.setState({
+      time: mins
+    });
     this.timerID = setInterval(() => this.tick(), EXP_MIN);
     this.timer = setTimeout(() => {
       this.setState({
+        time: 0,
+        user: 3,
         text: 'You can get your clothes.'
       });
+      alert('Get your clothes.');
     }, EXP_MIN * mins);
   }
 
   tick() {
-    this.setState((prevState) => {
-      time: prevState.time + 1
-    })
+    if (this.state.time == 0) {
+      clearInterval(this.timerID);
+      return;
+    }
+    this.setState((prevState) => ({
+      time: prevState.time - 1
+    }));
   }
 
   handleClickOn(e) {
@@ -56,6 +66,10 @@ class Washer extends React.Component {
       alert('You\'ve already turn the washer on.');
       return;
     }
+    else if (this.state.user == 3) {
+      alert('You need to get the clothes out.');
+      return;
+    }
     this.setState({
       user: 2,
       text: 'Being used.'
@@ -67,16 +81,35 @@ class Washer extends React.Component {
     return;
   }
 
+  handelClickGetClothes(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (this.state.user == 2 || this.state.user == 1) {
+      alert('Don\' do this.');
+      return;
+    }
+    else if (this.state.user == 0) {
+      alert('No clothes in the washer.');
+    }
+    this.setState({
+      user: 0,
+      text: 'Not being used.'
+    });
+  }
+
   render() {
     return (
       <div className="Washer">
         Washer No.{this.props.order}
+        <p className="Clock">
+          Time: {this.state.time}
+        </p>
         <p>
           {this.state.text}
           <button className="On" onClick={this.handleClickOn} >On</button>
-        </p>
-        <p className="Clock">
-          Time: {this.state.time}
+          <button className="GetClothes" onClick={this.handelClickGetClothes}>
+            Get Clothes
+          </button>
         </p>
       </div>
     );
